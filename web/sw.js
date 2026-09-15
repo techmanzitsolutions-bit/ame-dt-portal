2a0c04de83e49112c643bfc48d2a6b2a173b1b29
techmanzitsolutions-bit <techmanzitsolutions@gmail.com>
2026-09-15 13:57:41 +0400

    Add AME portal service worker

---
+++ web/sw.js
@@ -0,0 +1,1 @@
+const CACHE='ame-dt-v1';const ASSETS=['./','index.html','admin.html','manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
